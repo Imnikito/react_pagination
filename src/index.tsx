3b0f5e84f0b479c537ebb353dbf2af2b81b74383
Nikita Kovalenko <nkovalenko.m@gmail.com>
2022-06-24 23:02:12 +0300

    Update React to v 18.2.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import './index.scss';
 import Demo from './Demo';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container!);
+root.render(<Demo />);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
